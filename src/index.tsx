2b22978d1bc4e88ae9b0b1edbcddd3d8aabd6ee1
Miesztex <mqzzzy@gmail.com>
2021-04-08 11:21:06 +0200

    set project template

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+const App = () => {
+	return (
+		<div>
+			<h1>Hello</h1>
+		</div>
+	);
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.querySelector('#root'));
